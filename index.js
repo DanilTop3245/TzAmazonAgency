90d6bb98db4c3e082924f798173130a5861967ef
DanilTop3245 <turasov2009@gmail.com>
2025-06-15 16:02:13 +0300

    Add files and fix bags.

--- index.js
+++ index.js
@@ -2,13 +2,11 @@ const cardsContainer = document.getElementById("cards");
 const leftArrow = document.querySelector(".arrow-left");
 const rightArrow = document.querySelector(".arrow-right");
 
-// Нажатие влево – показать предыдущую (вставить в конец)
 leftArrow.addEventListener("click", () => {
   const first = cardsContainer.firstElementChild;
   cardsContainer.appendChild(first);
 });
 
-// Нажатие вправо – показать следующую (вставить в начало)
 rightArrow.addEventListener("click", () => {
   const last = cardsContainer.lastElementChild;
   cardsContainer.prepend(last);
